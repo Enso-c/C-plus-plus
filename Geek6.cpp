3839a1c50712af86066091a786af0e5ba13ae9e6
Oleg <55031975+Enso-c@users.noreply.github.com>
2022-10-22 14:36:31 +0300

    Add files via upload

---
+++ Geek6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include<math.h>
+int i;
+void Arr(int* arr[], int size);
+int main()
+{
+	return 0;
+}
+void Arr(int* arr[], int size) 
+{
+	
+	arr[0] = 1;
+	for (int i = 1; i < size; i++)
+	{
+		arr[i] = arr[i+1] << 1;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << std::endl;
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << std::endl;
+	}
+	delete[] arr;
+}
+
+
